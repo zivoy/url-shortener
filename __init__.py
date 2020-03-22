738ce2047bde8e7f2bb4624218ec9c24ceaf16dd
zivoy <ziv132@gmail.com>
2020-03-22 04:00:14 -0400

    added basic login

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import random
 import re
 from datetime import datetime
@@ -6,10 +7,107 @@ from datetime import datetime
 import psycopg2
 import psycopg2.extensions
 import requests
-from flask import Flask, request, redirect, render_template, abort, jsonify
+from flask import Flask, request, redirect, render_template, abort, \
+    jsonify, g, session, render_template_string
+from flask_github import GitHub
 from requests.exceptions import Timeout
 
 app = Flask(__name__)
+app.secret_key = os.getenv("FlaskDevKey")
+app.config['GITHUB_CLIENT_ID'] = "76649f9c931b9cd7d396"
+app.config['GITHUB_CLIENT_SECRET'] = os.getenv("GithubClientSecret")
+
+address = "localhost"  # "192.168.86.30"
+
+github = GitHub(app)
+
+"""
+CREATE TABLE urls(
+shortend        VARCHAR(25) PRIMARY KEY  NOT NULL,
+url             TEXT                     NOT NULL,
+creation_date   TIMESTAMP,
+author          TEXT
+);
+
+CREATE TABLE users(
+id                  INTEGER  PRIMARY KEY  NOT NULL,
+github_access_token VARCHAR(255)          NOT NULL,
+github_id           INTEGER,
+github_login        VARCHAR(255),
+authority_level     SMALLINT NOT NULL CHECK (authority_level >= 0)
+);
+"""
+
+authority_levels = {0: "Not Logged in",
+                    1: "Basic Member",
+                    2: "Advanced Member"}
+
+
+class User:
+    query = 'SELECT id, github_access_token, github_id, github_login, authority_level ' \
+            'FROM users WHERE {colname}=%({colname})s'
+
+    def __init__(self, uid, github_access_token, github_id, github_login, authority_level):
+        self.id = uid
+        self.github_access_token = github_access_token
+        self.github_id = github_id
+        self.github_login = github_login
+        self.authority_level = authority_level
+
+    @staticmethod
+    def get_by_id(uid):
+        # g.curr.execute("SELECT id, github_access_token, github_id, github_login, authority_level"
+        #                " FROM users WHERE id=%(uid)s", {"uid": uid})
+        g.curr.execute(User.query.format(colname="id"), {"id": uid})
+
+        usr = g.curr.fetchone()
+        if usr is not None:
+            return User(*usr)
+
+    @staticmethod
+    def get_by_access_token(github_access_token):
+        # g.curr.execute("SELECT id, github_access_token, github_id, github_login, authority_level"
+        #                " FROM users WHERE github_access_token=%(github_access_token)s",
+        #                {"github_access_token": github_access_token})
+        g.curr.execute(User.query.format(colname="github_access_token"),
+                       {"github_access_token": github_access_token})
+
+        usr = g.curr.fetchone()
+        if usr is not None:
+            return User(*usr)
+
+    @staticmethod
+    def get_by_github_id(github_id):
+        g.curr.execute(User.query.format(colname="github_id"),
+                       {"github_id": github_id})
+
+        usr = g.curr.fetchone()
+        if usr is not None:
+            return User(*usr)
+
+    @staticmethod
+    def only_token(github_access_token):
+        g.curr.execute("SELECT COUNT(id) FROM users")
+        uid = g.curr.fetchone()[0]
+        return User(uid, github_access_token, None, None, 1)
+
+    def add_to_table(self):
+        g.curr.execute("INSERT INTO users (id, github_access_token, github_id, github_login, authority_level) VALUES "
+                       "(%(id)s::integer, %(github_access_token)s, %(github_id)s, "
+                       "%(github_login)s, %(authority_level)s::smallint);",
+                       {"id": self.id, "github_access_token": self.github_access_token, "github_id": self.github_id,
+                        "github_login": self.github_login, "authority_level": self.authority_level})
+        g.db.commit()
+
+    def update(self):
+        g.curr.execute("UPDATE users SET (github_access_token, github_id, github_login, authority_level)="
+                       "(%(github_access_token)s,%(github_id)s,%(github_login)s,%(authority_level)s::smallint)"
+                       "WHERE id=%(id)s::integer;",
+                       {"id": self.id, "github_access_token": self.github_access_token, "github_id": self.github_id,
+                        "github_login": self.github_login, "authority_level": self.authority_level})
+        g.db.commit()
+
+
 urlRE = re.compile(
     r'^(?:http|ftp)s?://'  # http:// or https://
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
@@ -30,24 +128,32 @@ def validate_url(url):
     return False
 
 
-def connect() -> psycopg2.extensions.connection:
-    return psycopg2.connect(host="localhost", database="shorten-urls", user="python", password="1111")
+@app.before_request
+def before_request():
+    g.db = psycopg2.connect(host=address, database="shorten-urls", user="python", password="1111")
+    g.curr = g.db.cursor()
+    g.user = None
+    if "user_id" in session:
+        g.user = User.get_by_id(session['user_id'])
 
 
-def exists(name: str, courser: psycopg2.extensions.cursor) -> bool:
-    courser.execute("SELECT EXISTS(SELECT 1 FROM urls WHERE shortened=%(name)s);", {"name": name})
-    return courser.fetchone()[0]
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+    g.curr.close()
 
 
-def get(name: str, courser: psycopg2.extensions.cursor) -> str:
-    courser.execute("SELECT url FROM urls WHERE shortened=%(name)s", {"name": name})
-    return courser.fetchone()[0]
+def url_exists(name: str) -> bool:
+    g.curr.execute("SELECT EXISTS(SELECT 1 FROM urls WHERE shortened=%(name)s);", {"name": name})
+    return g.curr.fetchone()[0]
 
 
-def add(connection: psycopg2.extensions.connection, shortened: str, url: str,
-        author: str = None, creation_date: datetime = None):
-    curr: psycopg2.extensions.cursor = connection.cursor()
+def get_url(name: str) -> str:
+    g.curr.execute("SELECT url FROM urls WHERE shortened=%(name)s", {"name": name})
+    return g.curr.fetchone()[0]
 
+
+def add_url(shortened: str, url: str, author: str = None, creation_date: datetime = None):
     # if author is None:
     #     author = "NULL"
 
@@ -56,54 +162,69 @@ def add(connection: psycopg2.extensions.connection, shortened: str, url: str,
         # else:
         creation_date = f"'{creation_date.strftime('%Y-%m-%d %H:%M:%S')}'"
 
-    curr.execute("INSERT INTO urls (shortened, url, creation_date, author) VALUES "
-                 "(%(shortened)s, %(url)s, %(creation_date)s, %(author)s)",
-                 {"shortened": shortened, "url": url, "creation_date": creation_date, "author": author})
-    connection.commit()
-    curr.close()
+    g.curr.execute("INSERT INTO urls (shortened, url, creation_date, author) VALUES "
+                   "(%(shortened)s, %(url)s, %(creation_date)s, %(author)s)",
+                   {"shortened": shortened, "url": url, "creation_date": creation_date, "author": author})
+    g.db.commit()
 
 
-def get_id(url, curr):
+def get_id(url):
     val = 0
     for i, j in enumerate(url):
         val += (i + 1) * ord(j) * 314159265359
     num = int(((val % 10 ** 10) - 1000) / 10 ** 10 * 10 ** 6) + 1000
     short = base64.urlsafe_b64encode(str(num).encode()).decode()
-    if not exists(short, curr):
+    if not url_exists(short):
         return short
-    if get(short, curr) == url:
+    if get_url(short) == url:
         return short
     while True:
         num = random.randint(1000, 999999)
         short = base64.urlsafe_b64encode(str(num).encode()).decode()
-        if not exists(short, curr):
+        if not url_exists(short):
             return short
 
 
-def get_table(curr: psycopg2.extensions.cursor, author: str = None):
+def get_table(author: str = None):
     if author is None:
-        curr.execute("SELECT * FROM urls")
+        g.curr.execute("SELECT shortened, url, author, creation_date FROM urls")
     else:
-        curr.execute("SELECT * FROM urls WHERE LOWER(author)=%(author)s", {"author": author.lower()})
-    return curr.fetchall()
+        g.curr.execute("SELECT shortened, url, author, creation_date FROM urls WHERE LOWER(author)=%(author)s",
+                       {"author": author.lower()})
+    return [{"short": i[0],
+             "url": i[1],
+             "author": i[2],
+             "date": i[3]} for i in g.curr.fetchall()]
 
 
 @app.route("/")
 def home_page():
-    return "Url Shortener<br/><a href='/list'>list shortened links</a><br/><a href='/new'>add new shortend link</a>"
+    t = "Url Shortener<br/>"
+    if g.user:
+        t += f"Level: {authority_levels[g.user.authority_level]}<br/>"
+        t += f'Hello! {g.user.github_login} <a href="{{{{ url_for("user") }}}}">Get user</a> ' \
+             '<a href="{{ url_for("logout") }}">Logout</a>'
+    else:
+        t += f"Level: {authority_levels[0]}<br/>"
+        t += 'Hello! <a href="{{ url_for("login") }}">Login</a>'
+    t += "<br/><a href='/list'>list shortened links</a><br/><a href='/new'>add new shortend link</a>"
+
+    return render_template_string(t)
+
+
+@github.access_token_getter
+def token_getter():
+    if g.user is not None:
+        return g.user.github_access_token
 
 
 @app.route("/<url>")
 def redirect_path(url):
-    connection = connect()
-    curr = connection.cursor()
-    if not exists(url, curr):
+    if not url_exists(url):
         returned_value = f"\"{url}\" is not valid"
     else:
-        long_url = get(url, curr)
+        long_url = get_url(url)
         returned_value = redirect(long_url, 301)
-    curr.close()
-    connection.close()
     return returned_value
 
 
@@ -123,33 +244,29 @@ def add_new():
     else:
         author = None
 
-    connection = connect()
-    curr = connection.cursor()
-
     if "short" in request.json and request.json["short"] is not None:
         short = request.json["short"]
-        if short in ["new", "list"]:
-            curr.close()
-            connection.close()
+        if short in ["new", "list", "login", "logout", "user", "github-callback"]:
             abort(401)
     else:
-        short = get_id(url, curr)
+        short = get_id(url)
 
-    if not exists(short, curr):
-        add(connection, short, url, author, datetime.now())
+    if not url_exists(short):
+        add_url(short, url, author, datetime.now())
     elif "short" in request.json and request.json["short"] is not None:
-        curr.close()
-        connection.close()
         abort(401)
-    curr.close()
-    connection.close()
     return jsonify({"id": short})
 
 
 @app.route("/n", methods=["GET"])
 @app.route("/new", methods=["GET"])
 def new_form():
-    return render_template("newentryfourm.html", baseUrl=request.url_root)
+    author = "Unregistered"
+    level = 0
+    if g.user:
+        author = g.user.github_login
+        level = g.user.authority_level
+    return render_template("newentryfourm.html", baseUrl=request.url_root, author=author, authority_level=level)
     # "form thing " + str(request.args.get("author"))
 
 
@@ -163,26 +280,79 @@ def list_urls():
     else:
         author = request.json['author']
 
-    connection = connect()
-    curr = connection.cursor()
+    table = get_table(author)
 
-    tbl = get_table(curr, author)
-
-    table = [{"short": i[0],
-              "url": i[1],
-              "author": i[3],
-              "date": i[2]} for i in tbl]
-
-    curr.close()
-    connection.close()
     return jsonify(table)
 
 
 @app.route("/list")
 def render_table():
-    author = request.args.get("author")
+    authority_level = 0
+    author = None
+    if g.user:
+        authority_level = g.user.authority_level
+        author = g.user.github_login
+    if author is None:
+        author = "Unregistered"
+
+    if authority_level >= 2:
+        auth_arg = request.args.get("author")
+        if auth_arg is not None:
+            author = auth_arg
+
+    if author == "*":
+        author = None
+
     res = requests.post(request.url_root + 'api/list', json={"author": author})
     return render_template("all columns table.html", name=author, table=res.json())
+
+
+@app.route('/login')
+def login():
+    if session.get('user_id', None) is None:
+        return github.authorize()
+    else:
+        return 'Already logged in'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect("/")
+
+
+@app.route('/user')
+def user():
+    return jsonify(github.get('/user'))
+
+
+@app.route('/github-callback')
+@github.authorized_handler
+def authorized(access_token):
+    next_url = request.args.get('next') or "/"
+    if access_token is None:
+        return redirect(next_url)
+
+    g.user = User.only_token(access_token)
+    github_user = github.get('/user')
+
+    curr_user = User.get_by_github_id(github_user['id'])
+    if curr_user is None:
+        curr_user = User.only_token(access_token)
+        curr_user.add_to_table()
+
+    curr_user.github_access_token = access_token
+
+    # Not necessary to get these details here
+    # but it helps humans to identify users easily.
+    g.user = curr_user
+    curr_user.github_id = github_user['id']
+    curr_user.github_login = github_user['login']
+
+    g.user.update()
+
+    session['user_id'] = curr_user.id
+    return redirect(next_url)
 
 
 if __name__ == '__main__':
